57ea1f2be9235c0463c357ab8a1950114e6f6617
Rawat890 <rohanrawat7508@gmail.com>
2026-06-03 20:08:38 +0530

    chore: success console added for db connection

--- src/db/db.js
+++ src/db/db.js
@@ -7,7 +7,6 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(MONGO_URI);
-
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
