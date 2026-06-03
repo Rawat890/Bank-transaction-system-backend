8083a42e57a3e607169ba843bcbd9722a496569a
Rawat890 <rohanrawat7508@gmail.com>
2026-06-03 11:10:35 +0530

    feat: connectDB function created to connected to database

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import { MONGO_URI } from '../config/config.js';
+import { promises as dns } from 'node:dns';
+
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // exits the process with failure code to avoid running server without database connection
+  }
+};
+
+export default connectDB;
