33da6ce20527138258115511861d390b9ca8f5f0
Rawat890 <rohanrawat7508@gmail.com>
2026-06-04 12:01:59 +0530

    feat: authMiddleWare function created to check if the user is valid

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import { JWT_SECRET } from '../config/config.js';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/user.model.js';
+
+export async function authMiddleware(req, res, next) {
+ // extracting token from cookies or request headers
+ const token = req.cookies?.token || req.headers?.authorization?.split(' ')[1];
+ if (!token) {
+  return res.status(401).json({
+   message: "Unauthorized access, token is missing"
+  })
+ }
+
+ try {
+  const decoded = jwt.verify(token, JWT_SECRET);  // decodes the token and verifies
+  const user = await userModel.findById(decoded.userId); // retrieves user from database
+  req.user = user; //attaches user object to request
+
+  return next();
+ } catch (error) {
+  console.error("Error in auth middleware: ", error);
+  return res.status(401).json({
+   message: "Unauthorized access, token is invalid"
+  })
+ }
+}
