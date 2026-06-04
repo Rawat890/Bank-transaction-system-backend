d38028b59b4ba57d84f991db70fd2340fd0268b7
Rawat890 <rohanrawat7508@gmail.com>
2026-06-04 12:05:29 +0530

    chore: accountRouter and cookie parser added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/auth.routes.js';
+import accountRouter from './routes/account.routes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.json());
@@ -8,9 +10,13 @@ app.use(express.json());
 
 app.use(cors());
 // Enable cors for all routes. This allows cross-origin requests from any domain, which is useful for frontend-backend communication during development.
-
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
-//mount authRouter on /app/auth path. All routes defined in authRouter will be prefixed with /api/auth. For example, if authRouter has a route defined as POST /register, it will be accessible at /api/auth/register
+//mount authRouter on /api/auth path. All routes defined in authRouter will be prefixed with /api/auth. For example, if authRouter has a route defined as POST /register, it will be accessible at /api/auth/register
+
+app.use("/api/accounts", accountRouter);
+//mount accountRouter on /api/accounts path. All routes defined in accountRouter will be prefixed with /api/accounts
+
 
 export default app;
