e3e6e34fc92aad94c31672aa54863444ea6c61d3
Rawat890 <rohanrawat7508@gmail.com>
2026-06-05 19:01:43 +0530

    chore: authSystemUserMiddleware created for system user protected route

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -24,3 +24,32 @@ export async function authMiddleware(req, res, next) {
   })
  }
 }
+
+export async function authSystemUserMiddleware(req, res, next) {
+ // extracting token from cookies or request headers
+ const token = req.cookies?.token || req.headers?.authorization?.split(' ')[1];
+ if (!token) {
+  return res.status(401).json({
+   message: "Unauthorized access, token is missing"
+  })
+ }
+
+ try {
+  const decoded = jwt.verify(token, JWT_SECRET);  // decodes the token and verifies
+  const user = await userModel.findById(decoded.userId).select("+systemUser"); // retrieves user from database
+
+  if (!user.systemUser) {
+   return res.status(403).json({
+    message: "Forbidden access, not a system user"
+   })
+  }
+  req.user = user; //attaches user object to request
+
+  return next();
+ } catch (error) {
+  console.error("Error in auth middleware: ", error);
+  return res.status(401).json({
+   message: "Unauthorized access, token is invalid"
+  })
+ }
+}
